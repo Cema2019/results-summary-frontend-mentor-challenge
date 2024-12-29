c390314062c7df0f875af0366dd6007325a16172
Carlos M <53409854+Cema2019@users.noreply.github.com>
2024-12-29 14:33:36 -0600

    update tsconfig.json

--- components/summaryItems.tsx
+++ components/summaryItems.tsx
@@ -1,5 +1,5 @@
 import { Stack, Card, Box, Typography } from "@mui/material";
-import summaryData from "../data/summaryData.json";
+import summaryData from "../data/data.json";
 
 type SummaryItemType = {
   category: string;
